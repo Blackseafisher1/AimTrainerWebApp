8124025d158db074d89e2f34b12a3f9351de6beb
Blackseafisher1 <nice.ege.cool@gmail.com>
2025-07-16 16:29:42 +0200

    Update positionWorker.js

--- positionWorker.js
+++ positionWorker.js
@@ -17,12 +17,9 @@ function isOverlapping(pos1, pos2, minDistance) {
 
 // Generiert zufällige Position mit Randabstand
 function getRandomPosition(size, width, height) {
-   
-     const margin = size*2;
-
     return {
-      left: margin + fastRandom() * (width - size - 2 * margin),
-      top: margin + fastRandom() * (height - size - 2 * margin)
+      left: margin + fastRandom() * (width - size),
+      top: margin + fastRandom() * (height - size)
     };
   }
 
